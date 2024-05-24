11c8121f69fbc2d6fe9fcde766a04bc6f02c6eeb
H0onnn <dukei2017@naver.com>
2024-05-24 16:56:58 +0900

    init : 프로젝트 세팅

---
+++ app/api/user/route.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const res = await prisma.user.create({
+    data: {},
+  });
+
+  return Response.json("User created!");
+}
