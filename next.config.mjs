253933deac377a8737027197896853fabf6f7e43
H0onnn <dukei2017@naver.com>
2024-05-26 23:27:17 +0900

    chore : 이미지 호스트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "*",
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
