1d4a5777420ee82d5cde3477fc0262a52165b91c
H0onnn <dukei2017@naver.com>
2024-05-26 23:33:47 +0900

    feat : cookie server action 함수 추가

---
+++ app/shared/actions/cookie.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getCookie = async (name: string) => {
+  const cookieStore = cookies();
+  return cookieStore.get(name);
+};
+
+export const setCookie = async ({
+  name,
+  value,
+  expires,
+}: {
+  name: string;
+  value: string;
+  expires?: number; // 단위: ms
+}) => {
+  cookies().set({
+    name,
+    value,
+    httpOnly: true,
+    path: "/",
+    expires,
+  });
+};
+
+export const deleteCookie = async (key: string) => {
+  cookies().delete(key);
+};
