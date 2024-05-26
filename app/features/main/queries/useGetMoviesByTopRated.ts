82182cefc8140d41002f6e8bfe7aa01dc3530e9b
H0onnn <dukei2017@naver.com>
2024-05-26 23:34:11 +0900

    feat : useGetMovies 훅 추가

---
+++ app/features/main/queries/useGetMoviesByTopRated.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { MovieList } from "@/app/shared/types";
+
+const TMDB_API_URL = process.env.NEXT_PUBLIC_TMDB_API_URL;
+const TMDB_ACCESS_TOKEN = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN;
+
+export const fetchMovies = async () => {
+  try {
+    const response = await fetch(
+      `${TMDB_API_URL}top_rated?language=ko-KR&region=KR&sort_by=vote_average.desc&video=true&adult=false`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+          Accept: "application/json",
+        },
+      },
+    );
+
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const useGetMoviesByTopRated = () => {
+  const { data, isFetching } = useQuery<MovieList>({
+    queryKey: ["movies", "top_rated"],
+    queryFn: fetchMovies,
+    staleTime: 1000 * 60 * 60 * 24, // 24시간
+  });
+
+  return { movieListByTopRated: data, isFetching };
+};
