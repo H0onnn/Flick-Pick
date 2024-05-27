b744208caab296c5a8f13be9c2e537d08b4132bf
H0onnn <dukei2017@naver.com>
2024-05-27 17:54:47 +0900

    feat : 로그인 버튼 조건부

--- app/shared/components/blocks/main-header.tsx
+++ app/shared/components/blocks/main-header.tsx
@@ -11,12 +11,23 @@ import { LoginModal } from "@/app/features/auth/components";
 export const MainHeader = () => {
   const { data: session, status } = useSession();
 
-  console.log(session, status);
-
   return (
     <Header
       leftSlot={<Image src={IMAGES.LOGO} alt="로고" width={100} height={40} />}
-      rightSlot={<LoginModal />}
+      rightSlot={
+        status === "authenticated" ? (
+          <div className="w-[34px] h-[34px] rounded-full">
+            <Image
+              src={session.user?.image!}
+              alt="프로필 사진"
+              fill={true}
+              className="object-cover rounded-full"
+            />
+          </div>
+        ) : (
+          <LoginModal />
+        )
+      }
     >
       {null}
     </Header>
