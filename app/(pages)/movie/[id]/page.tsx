024391f8b0120ecde632afd327556b609aa0bc04
H0onnn <dukei2017@naver.com>
2024-05-27 17:45:12 +0900

    feat : 영화 상세 페이지

---
+++ app/(pages)/movie/[id]/page.tsx
@@ -0,0 +1,35 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
+import { fetchMovie } from "@/app/features/main/queries";
+
+import { PageLayout, MainHeader } from "@/app/shared/components";
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["movie", params.id],
+    queryFn: () => fetchMovie(params.id),
+    staleTime: Infinity,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <PageLayout header={<MainHeader />}>
+        <section className="py-5">
+          <h1>Movie Detail</h1>
+        </section>
+      </PageLayout>
+    </HydrationBoundary>
+  );
+}
