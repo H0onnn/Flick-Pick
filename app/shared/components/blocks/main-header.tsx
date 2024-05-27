28690966e6f642245d21d09b73d4fc4c7545d0c1
H0onnn <dukei2017@naver.com>
2024-05-27 17:41:33 +0900

    feat : 헤더 로그인 모달 적용

--- app/shared/components/blocks/main-header.tsx
+++ app/shared/components/blocks/main-header.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { Header } from "..";
-import { Flex, Button } from "@/app/shared/components";
+import { useSession } from "next-auth/react";
+
+import Image from "next/image";
+
+import IMAGES from "@/public/images";
+import { Header } from "@/app/shared/components";
+import { LoginModal } from "@/app/features/auth/components";
 
 export const MainHeader = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session, status);
+
   return (
     <Header
-      leftSlot="LOGO"
-      rightSlot={
-        <Flex className="gap-2">
-          <Button variant="outline">로그인</Button>
-          <Button>회원가입</Button>
-        </Flex>
-      }
+      leftSlot={<Image src={IMAGES.LOGO} alt="로고" width={100} height={40} />}
+      rightSlot={<LoginModal />}
     >
       {null}
     </Header>
