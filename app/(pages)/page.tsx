01f8b1533bbd52304d0e72d6884522df0db720a5
H0onnn <dukei2017@naver.com>
2024-05-27 11:22:24 +0900

    refactor : 메인 배너 섹션 컴포넌트 분리

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -6,8 +6,8 @@ import {
 
 import { fetchMovies } from "../features/main/queries";
 
-import { PageLayout, Flex, Button } from "@/app/shared/components";
-import { MainBanner } from "@/app/features/main/components";
+import { PageLayout, MainHeader } from "@/app/shared/components";
+import { BannerSection } from "@/app/features/main/components";
 
 export default async function Page() {
   const queryClient = new QueryClient();
@@ -20,23 +20,8 @@ export default async function Page() {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <PageLayout
-        header={{
-          leftSlot: "logo",
-          rightSlot: (
-            <Flex className="gap-2">
-              <Button variant="outline">로그인</Button>
-              <Button>회원가입</Button>
-            </Flex>
-          ),
-        }}
-      >
-        <section className="py-5">
-          <Flex direction="column" className="gap-2">
-            <p className="head2 font-bold">TMDB 순위</p>
-            <MainBanner />
-          </Flex>
-        </section>
+      <PageLayout header={<MainHeader />}>
+        <BannerSection />
       </PageLayout>
     </HydrationBoundary>
   );
