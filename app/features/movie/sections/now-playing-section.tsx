7c97371c7f607ace98ccba4000ab7269d5898079
H0onnn <dukei2017@naver.com>
2024-05-28 23:54:03 +0900

    rename : 폴더명 변경

--- app/features/movie/sections/now-playing-section.tsx
+++ app/features/movie/sections/now-playing-section.tsx
@@ -1,9 +1,9 @@
 import { Flex } from "@/app/shared/components";
-import { NowPlayingCarousel } from "@/app/features/main/components";
+import { NowPlayingCarousel } from "@/app/features/movie/components";
 
 export const NowPlayingSection = () => {
   return (
-    <section className="pt-5">
+    <section className="pt-8">
       <Flex direction="column" className="gap-2">
         <p className="head2 font-bold">현재 상영작</p>
         <NowPlayingCarousel />
--- app/features/movie/sections/top-rated-section.tsx
+++ app/features/movie/sections/top-rated-section.tsx
@@ -1,9 +1,9 @@
 import { Flex } from "@/app/shared/components";
-import { TopRatedCarousel } from "@/app/features/main/components";
+import { TopRatedCarousel } from "@/app/features/movie/components";
 
 export const TopRatedSection = () => {
   return (
-    <section className="pt-5">
+    <section className="pt-8">
       <Flex direction="column" className="gap-2">
         <p className="head2 font-bold">TMDB 순위</p>
         <TopRatedCarousel />
