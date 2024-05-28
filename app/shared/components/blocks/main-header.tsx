9882d836f8a0db61cc97d6991646693a18234f9b
H0onnn <dukei2017@naver.com>
2024-05-28 23:58:03 +0900

    feat : search-input & Link 적용

--- app/shared/components/blocks/main-header.tsx
+++ app/shared/components/blocks/main-header.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { Session } from "next-auth";
 import Image from "next/image";
-
+import Link from "next/link";
 import IMAGES from "@/public/images";
-import { Header } from "@/app/shared/components";
+import { Flex, Header } from "@/app/shared/components";
 import { LoginModal } from "@/app/features/auth/components";
+import { SearchInput } from "@/app/features/search/components";
 
 export const MainHeader = () => {
   const { data: session, status } = useSession();
@@ -16,14 +19,19 @@ export const MainHeader = () => {
       leftSlot={<Image src={IMAGES.LOGO} alt="로고" width={100} height={40} />}
       rightSlot={
         status === "authenticated" ? (
-          <div className="w-[34px] h-[34px] rounded-full">
-            <Image
-              src={session.user?.image!}
-              alt="프로필 사진"
-              fill={true}
-              className="object-cover rounded-full"
-            />
-          </div>
+          <Flex align="center" justify="center" className="gap-4">
+            <SearchInput />
+            <Link href={`user/${session.user?.id}`}>
+              <div className="relative w-[34px] h-[34px] rounded-full sm-max:hidden border border-border border-solid">
+                <Image
+                  src={session.user?.image!}
+                  alt="프로필 사진"
+                  fill={true}
+                  className="object-cover rounded-full"
+                />
+              </div>
+            </Link>
+          </Flex>
         ) : (
           <LoginModal />
         )
