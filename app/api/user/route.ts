24073202c7ffd16cd4f2276c6ccfe5b6e95ee094
H0onnn <dukei2017@naver.com>
2024-05-30 17:12:14 +0900

    remove : 불필요 파일 삭제

--- app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/app/shared/lib/prisma";
-
-export async function GET() {
-  const res = await prisma.user.create({
-    data: {},
-  });
-
-  return Response.json("User created!");
-}
