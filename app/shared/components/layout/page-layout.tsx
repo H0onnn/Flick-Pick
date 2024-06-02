5fc0992ba6213cf57b29efdaa2f51aef3faed6f0
H0onnn <dukei2017@naver.com>
2024-06-02 14:03:04 +0900

    style : paddingTop 조건부

--- app/shared/components/layout/page-layout.tsx
+++ app/shared/components/layout/page-layout.tsx
@@ -4,19 +4,25 @@ type PageLayoutProps = {
   children: React.ReactNode;
   header?: React.ReactNode;
   className?: string;
+  isPaddingTop?: boolean;
 };
 
 export const PageLayout = ({
   children,
   header,
   className,
+  isPaddingTop = true,
 }: PageLayoutProps) => {
   const paddingBottom = header !== null ? "pb-[83px]" : "pb-0";
-  const paddingTop = header !== null ? "pt-[62px]" : "pt-0";
+  const paddingTop = header !== null && isPaddingTop ? "pt-[62px]" : "pt-0";
 
   return (
     <div
-      className={cn([paddingTop, paddingBottom, "min-h-screen"])}
+      className={cn([
+        paddingTop,
+        paddingBottom,
+        "min-h-screen container px-4 sm:px-6 lg:px-8",
+      ])}
       vaul-drawer-wrapper="" // eslint-disable-line
     >
       {header}
