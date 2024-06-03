08dd881c7144531ea8d7253911f3c23ff0ae7100
H0onnn <dukei2017@naver.com>
2024-06-03 10:23:07 +0900

    feat : star-rating 컴포넌트 추가

--- app/public/icons/index.ts
+++ app/public/icons/index.ts
@@ -1,7 +1,13 @@
 import KAKAO from "./kakao.svg";
+import EMPTY_STAR from "./emptyStar.svg";
+import FULL_STAR from "./fillStar.svg";
+import HALF_STAR from "./halfStar.svg";
 
 const ICONS = {
   KAKAO,
+  EMPTY_STAR,
+  FULL_STAR,
+  HALF_STAR,
 } as const;
 
 export type IconType = keyof typeof ICONS;
---
+++ app/shared/components/rating/star-rating.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import ICONS from "@/app/public/icons";
+import { Flex } from "@/app/shared/components";
+
+interface Props {
+  // eslint-disable-next-line no-unused-vars
+  onChange: (value: number) => void;
+  size?: number;
+}
+
+const starsArray = Array.from({ length: 5 });
+
+const calculateStarValue = (
+  index: number,
+  e: React.MouseEvent<HTMLSpanElement>,
+) => {
+  const rect = e.currentTarget.getBoundingClientRect();
+  const offsetX = e.clientX - rect.left;
+  return offsetX < rect.width / 2 ? index + 0.5 : index + 1;
+};
+
+export const StarRating = ({ size = 32, onChange }: Props) => {
+  const [rating, setRating] = useState<number | null>(null);
+  const [hover, setHover] = useState<number | null>(null);
+
+  const updateStarRating = (value: number) => {
+    setRating(value);
+    onChange(value);
+  };
+
+  const handleStarClick = (
+    index: number,
+    e: React.MouseEvent<HTMLSpanElement>,
+  ) => {
+    const selectedValue = calculateStarValue(index, e);
+    updateStarRating(selectedValue);
+  };
+
+  const handleMouseOver = (
+    index: number,
+    e: React.MouseEvent<HTMLSpanElement>,
+  ) => {
+    const hoverValue = calculateStarValue(index, e);
+    setHover(hoverValue);
+  };
+
+  const renderStar = (index: number) => {
+    const value = hover !== null ? hover : rating;
+
+    if (value === null) {
+      return (
+        <Image
+          src={ICONS.EMPTY_STAR}
+          className="w-full h-full"
+          alt="empty star"
+        />
+      );
+    }
+
+    const starType =
+      value > index + 0.5 ? "full" : value > index ? "half" : "empty";
+
+    const starIcons = {
+      full: (
+        <Image
+          src={ICONS.FULL_STAR}
+          className="w-full h-full"
+          alt="full star"
+        />
+      ),
+      half: (
+        <Image
+          src={ICONS.HALF_STAR}
+          className="w-full h-full"
+          alt="half star"
+        />
+      ),
+      empty: (
+        <Image
+          src={ICONS.EMPTY_STAR}
+          className="w-full h-full"
+          alt="empty star"
+        />
+      ),
+    };
+
+    return starIcons[starType];
+  };
+
+  return (
+    <Flex className="gap-1">
+      {starsArray.map((_, index) => (
+        <span
+          key={index}
+          style={{ width: size, height: size }}
+          onClick={(e) => handleStarClick(index, e)}
+          onMouseMove={(e) => handleMouseOver(index, e)}
+          onMouseLeave={() => setHover(null)}
+        >
+          {renderStar(index)}
+        </span>
+      ))}
+    </Flex>
+  );
+};
