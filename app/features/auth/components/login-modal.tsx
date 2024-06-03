7df2fd3f7f14032e4dc7ebd8f952e103e177ea3d
H0onnn <dukei2017@naver.com>
2024-06-03 17:42:48 +0900

    style : 스크롤 여부에 따른 text color 변경

--- app/features/auth/components/login-modal.tsx
+++ app/features/auth/components/login-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useScroll } from "@/app/shared/hooks";
 import Image from "next/image";
 import IMAGES from "@/app/public/images";
 import ICONS from "@/app/public/icons";
@@ -16,6 +17,8 @@ import { toast } from "sonner";
 import { signIn } from "next-auth/react";
 
 export const LoginModal = () => {
+  const { isScrolled } = useScroll();
+
   const handleLogin = async (provider: string) => {
     try {
       await signIn(provider);
@@ -30,32 +33,42 @@ export const LoginModal = () => {
 
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="link" className="head6sb">
-          로그인/가입
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[488px] p-12">
-        <DialogHeader className="flex-col gap-4 items-center justify-center">
-          <Image src={IMAGES.LOGO} alt="로고" width={150} height={50} />
-          <p className="head2 text-center">
-            플릭픽과 함께,
-            <br />
-            당신의 최애 영화를 찾아보세요!
-          </p>
-        </DialogHeader>
+      <form>
+        <DialogTrigger asChild>
+          <Button
+            variant="link"
+            className={`head6sb ${isScrolled === false && "text-white"}`}
+          >
+            로그인/가입
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[488px] p-12">
+          <DialogHeader className="flex-col gap-4 items-center justify-center">
+            <Image src={IMAGES.LOGO} alt="로고" width={150} height={50} />
+            <p className="head2 text-center">
+              플릭픽과 함께,
+              <br />
+              당신의 최애 영화를 찾아보세요!
+            </p>
+          </DialogHeader>
 
-        <Button
-          type="submit"
-          className="bg-kakao mt-6 py-6 hover:bg-kakao/70"
-          onClick={() => handleLogin("kakao")}
-        >
-          <Flex align="center" justify="center" className="w-full gap-2">
-            <Image src={ICONS.KAKAO} alt="카카오 로고" width={24} height={24} />
-            <p className="text-black">카카오로 시작하기</p>
-          </Flex>
-        </Button>
-      </DialogContent>
+          <Button
+            type="submit"
+            className="bg-kakao mt-6 py-6 hover:bg-kakao/70"
+            onClick={() => handleLogin("kakao")}
+          >
+            <Flex align="center" justify="center" className="w-full gap-2">
+              <Image
+                src={ICONS.KAKAO}
+                alt="카카오 로고"
+                width={24}
+                height={24}
+              />
+              <p className="text-black">카카오로 시작하기</p>
+            </Flex>
+          </Button>
+        </DialogContent>
+      </form>
     </Dialog>
   );
 };
