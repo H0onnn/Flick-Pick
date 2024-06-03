456f00371dadc9c025a62f8e8e06f923d032304a
H0onnn <dukei2017@naver.com>
2024-06-03 17:11:15 +0900

    feat : 영화 상세 & 관련 영화 함수 추가

--- app/features/movie/queries/actions.ts
+++ app/features/movie/queries/actions.ts
@@ -1,13 +1,14 @@
 "use server";
 
-import { MovieList } from "@/app/shared/types";
+import { MovieDetail, MovieList } from "@/app/shared/types";
 
 const TMDB_API_URL = process.env.TMDB_API_URL;
 const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 
+// 현재 상영작
 export const getMoviesByNowPlaying = async (): Promise<MovieList> => {
   const response = await fetch(
-    `${TMDB_API_URL}now_playing?language=ko-KR&region=KR&sort_by=vote_average.desc&video=true&adult=false`,
+    `${TMDB_API_URL}now_playing?language=ko-KR&region=KR&sort_by=vote_average.desc&adult=false`,
     {
       method: "GET",
       headers: {
@@ -22,12 +23,13 @@ export const getMoviesByNowPlaying = async (): Promise<MovieList> => {
     throw new Error("현재 상영작 목록을 불러오는데 실패했습니다.");
   }
 
-  return response.json();
+  return await response.json();
 };
 
+// TMDB 인기순
 export const getMoviesByTopRated = async (): Promise<MovieList> => {
   const response = await fetch(
-    `${TMDB_API_URL}top_rated?language=ko-KR&region=KR&sort_by=vote_average.desc&video=true&adult=false`,
+    `${TMDB_API_URL}top_rated?language=ko-KR&region=KR&sort_by=vote_average.desc&adult=false`,
     {
       method: "GET",
       headers: {
@@ -42,12 +44,13 @@ export const getMoviesByTopRated = async (): Promise<MovieList> => {
     throw new Error("TMDB 순위 목록을 불러오는데 실패했습니다.");
   }
 
-  return response.json();
+  return await response.json();
 };
 
+// 상영 예정작
 export const getMoviesByUpcoming = async (): Promise<MovieList> => {
   const response = await fetch(
-    `${TMDB_API_URL}upcoming?language=ko-KR&region=KR&sort_by=vote_average.desc&video=true&include_adult=false`,
+    `${TMDB_API_URL}upcoming?language=ko-KR&region=KR&sort_by=vote_average.desc&include_adult=false`,
     {
       method: "GET",
       headers: {
@@ -62,5 +65,50 @@ export const getMoviesByUpcoming = async (): Promise<MovieList> => {
     throw new Error("상영 예정작 목록을 불러오는데 실패했습니다.");
   }
 
-  return response.json();
+  return await response.json();
+};
+
+// 영화 상세 정보
+export const getMovieDetail = async (id: string): Promise<MovieDetail> => {
+  const response = await fetch(
+    `${TMDB_API_URL}${id}?append_to_response=credits&language=ko-KR`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+        Accept: "application/json",
+      },
+    },
+  );
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error("영화 상세 정보를 불러오는데 실패했습니다.");
+  }
+
+  return await response.json();
+};
+
+// 장르 id로 연관 검색
+export const getMoviesByGenre = async (
+  genreIds: number[],
+  page?: number,
+): Promise<MovieList> => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&language=ko-KR&page=${page}&sort_by=popularity.desc&with_genres=${genreIds.join(",")}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+        Accept: "application/json",
+      },
+    },
+  );
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error("장르별 영화 목록을 불러오는데 실패했습니다.");
+  }
+
+  return await response.json();
 };
--- app/features/movie/queries/index.ts
+++ app/features/movie/queries/index.ts
@@ -1,3 +1,4 @@
+export * from "./actions";
 export * from "./use-movie-detail";
 export * from "./use-get-movies-by-query";
 export * from "./use-get-movies-by-top-rated";
