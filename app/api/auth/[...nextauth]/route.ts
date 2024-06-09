e48d947091602aca79d437547f5babd6f8c09ac7
H0onnn <dukei2017@naver.com>
2024-06-09 23:24:30 +0900

    fix : as never

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/app/shared/lib/next-auth";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions) as never;
 
 export { handler as GET, handler as POST };
