8a59bb66c96cd6524fa87717c42de566f063d17f
H0onnn <dukei2017@naver.com>
2024-06-09 23:36:22 +0900

    refactor : 캐러셀 삭제

--- app/features/movie/sections/detail-credits-section.tsx
+++ app/features/movie/sections/detail-credits-section.tsx
@@ -1,14 +1,10 @@
-import { useMemo } from "react";
-import {
-  Flex,
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/app/shared/components";
+"use client";
+
+import { useMemo, useEffect } from "react";
+import { Flex } from "@/app/shared/components";
 import { CreditsCard } from "../components";
 import { MovieDetail } from "@/app/features/movie/models";
+import { saveMovie } from "../queries";
 
 export const DetailCreditsSection = ({
   movieDetail,
@@ -21,47 +17,40 @@ export const DetailCreditsSection = ({
     return movieDetail.credits.crew.find((crew) => crew.job === "Director");
   }, [movieDetail]);
 
-  if (!movieDetail) return null;
+  useEffect(() => {
+    const saveMovieDetail = async () => {
+      await saveMovie(movieDetail);
+    };
+
+    saveMovieDetail();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <section className="pt-12">
       <p className="head2 mb-2">출연/제작</p>
 
-      <Carousel
-        opts={{
-          align: "start",
-          watchDrag: true,
-        }}
-      >
-        <CarouselPrevious className="hidden sm:flex" />
-        <CarouselContent>
-          <CarouselItem>
-            <Flex className="flex flex-wrap" asChild>
-              <ul>
-                <li className="w-full sm:w-1/2 lg:w-1/4 mb-6">
-                  <CreditsCard
-                    crewName={director?.name ?? "감독 정보 없음"}
-                    job="감독"
-                    profilePath={director?.profile_path || ""}
-                  />
-                </li>
-                {movieDetail.credits.cast.slice(0, 7).map((cast) => (
-                  <li key={cast.id} className="w-full sm:w-1/2 lg:w-1/4 mb-6">
-                    <CreditsCard
-                      crewName={cast.name}
-                      job={`${
-                        cast.character.split("(")[1] ? "성우" : "배우"
-                      } • ${cast.character.split("(")[0]}`}
-                      profilePath={cast.profile_path || ""}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </Flex>
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselNext className="hidden sm:flex" />
-      </Carousel>
+      <Flex className="flex flex-wrap" asChild>
+        <ul>
+          <li className="w-full sm:w-1/2 lg:w-1/4 mb-6">
+            <CreditsCard
+              crewName={director?.name ?? "감독 정보 없음"}
+              job="감독"
+              profilePath={director?.profile_path || ""}
+            />
+          </li>
+          {movieDetail.credits.cast.slice(0, 7).map((cast) => (
+            <li key={cast.id} className="w-full sm:w-1/2 lg:w-1/4 mb-6">
+              <CreditsCard
+                crewName={cast.name}
+                job={`${
+                  cast.character.split("(")[1] ? "성우" : "배우"
+                } • ${cast.character.split("(")[0]}`}
+                profilePath={cast.profile_path || ""}
+              />
+            </li>
+          ))}
+        </ul>
+      </Flex>
     </section>
   );
 };
