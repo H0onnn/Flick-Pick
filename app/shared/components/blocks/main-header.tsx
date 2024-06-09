c6648d655653a3d36e64ec7646abee9605796386
H0onnn <dukei2017@naver.com>
2024-06-09 23:41:08 +0900

    fix : import 경로 수정

--- app/shared/components/blocks/main-header.tsx
+++ app/shared/components/blocks/main-header.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/shared/lib/next-auth";
 import Image from "next/image";
 import Link from "next/link";
 import IMAGES from "@/app/public/images";
