c98d0c39b56d33874726f2fecbcc6fad476626a4
H0onnn <dukei2017@naver.com>
2024-06-09 23:16:13 +0900

    refactor : remove react-query

--- app/features/movie/queries/index.ts
+++ app/features/movie/queries/index.ts
@@ -1,6 +1,2 @@
 export * from "./actions";
-export * from "./use-movie-detail";
 export * from "./use-get-movies-by-query";
-export * from "./use-get-movies-by-top-rated";
-export * from "./use-get-movies-by-now-playing";
-export * from "./use-get-movies-by-upcoming";
