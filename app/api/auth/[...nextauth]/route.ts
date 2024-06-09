cfc62dff5fc11fc0e037c965a3bf3d00f8671b9d
H0onnn <dukei2017@naver.com>
2024-06-09 23:11:09 +0900

    refactor : authOption 분리

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,5 @@
-import NextAuth, { type AuthOptions } from "next-auth";
-import KakaoProvider from "next-auth/providers/kakao";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/app/shared/lib/prisma";
-
-export const authOptions: AuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID as string,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt" as const,
-  },
-  callbacks: {
-    session: ({ session, token }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: token.sub,
-      },
-    }),
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/shared/lib/next-auth";
 
 const handler = NextAuth(authOptions);
 
