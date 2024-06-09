4686e8bf38958255d7ecb6c501a4c06db69ef22a
H0onnn <dukei2017@naver.com>
2024-06-09 22:44:59 +0900

    refactor : 섹션명 변경

--- app/features/review/sections/main-review-section.tsx
+++ app/features/review/sections/main-review-section.tsx
@@ -5,7 +5,7 @@ export const MainReviewSection = () => {
   return (
     <section className="pt-8">
       <Flex direction="column" className="gap-2">
-        <p className="head2">요즘 뜨는 리뷰</p>
+        <p className="head2">따끈따끈 최신 리뷰</p>
         <MainReviewCarousel />
       </Flex>
     </section>
