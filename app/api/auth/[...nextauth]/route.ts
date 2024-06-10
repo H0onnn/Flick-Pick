d8cf2b2afb4f3335cfb4aad9fc7cf70a92d08f65
H0onnn <dukei2017@naver.com>
2024-06-10 13:06:49 +0900

    refactor : authOptions 타입

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/app/shared/lib/next-auth";
+import { AuthOptions } from "next-auth";
 
-const handler = NextAuth(authOptions) as never;
+const handler = NextAuth(authOptions as AuthOptions);
 
 export { handler as GET, handler as POST };
