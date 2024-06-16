df8384d0258bc9bb71403c865431c1e8d2a52c35
H0onnn <dukei2017@naver.com>
2024-06-16 13:25:53 +0900

    feat : tmdb, youtube 로고 추가

--- app/public/images/index.ts
+++ app/public/images/index.ts
@@ -1,7 +1,11 @@
 import LOGO from "./flick-pick-logo.png";
+import TMDB_LOGO from "./tmdb.svg";
+import YOUTUBE_LOGO from "./Youtube.png";
 
 const IMAGES = {
   LOGO,
+  TMDB_LOGO,
+  YOUTUBE_LOGO,
 } as const;
 
 export type ImageType = keyof typeof IMAGES;
