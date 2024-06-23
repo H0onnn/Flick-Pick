fc98bedfec33b98d77afc090a582a66c84da2fe3
H0onnn <dukei2017@naver.com>
2024-06-23 21:15:34 +0900

    feat : 마이페이지 구현

--- app/(pages)/user/info/[id]/page.tsx
+++ app/(pages)/user/info/[id]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div>user page</div>;
+import { PageLayout, MainHeader, Flex } from "@/app/shared/components";
+
+import {
+  InventorySection,
+  UserInfoSection,
+} from "@/app/features/user/sections";
+import { redirectTo } from "@/app/shared/actions";
+
+interface Props {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  if (!searchParams.category) {
+    await redirectTo(`/user/info/${params.id}?category=review`);
+  }
+
+  return (
+    <PageLayout header={<MainHeader />} className="pt-5">
+      <Flex className="gap-4">
+        <UserInfoSection />
+
+        <InventorySection category={searchParams?.category as string} />
+      </Flex>
+    </PageLayout>
+  );
 }
