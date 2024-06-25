c4b9c71a296cfae78cd861b1a27c963992a169ad
H0onnn <dukei2017@naver.com>
2024-06-25 11:03:06 +0900

    comment : 주석처리 해제

--- app/features/movie/sections/detail-trailer-section.tsx
+++ app/features/movie/sections/detail-trailer-section.tsx
@@ -9,35 +9,35 @@ export const DetailTrailerSection = async ({
   movieId: string;
 }) => {
   const movieDetail = await getMovieDetail(movieId);
-  //   const trailer = await getMovieTrailer(movieDetail.title);
+  const trailer = await getMovieTrailer(movieDetail.title);
 
-  //   if (trailer.error) {
+  if (trailer.error) {
+    return (
+      <section className="pt-6">
+        <p className="head2 mb-2">예고편</p>
+
+        <NonDataFallback
+          icon={<Youtube size={72} className="text-gray-500" />}
+          fallbackText={
+            <>
+              현재 예고편 정보를 불러올 수 없습니다.
+              <br />
+              {`다음에 다시 시도해주세요. :(`}
+            </>
+          }
+          className="h-80 sm:h-96 md:h-[450px] lg:h-[550px]"
+        />
+      </section>
+    );
+  }
+
+  if (!trailer.items) return null;
+
   return (
     <section className="pt-6">
       <p className="head2 mb-2">예고편</p>
 
-      <NonDataFallback
-        icon={<Youtube size={72} className="text-gray-500" />}
-        fallbackText={
-          <>
-            현재 예고편 정보를 불러올 수 없습니다.
-            <br />
-            {`다음에 다시 시도해주세요. :(`}
-          </>
-        }
-        className="h-80 sm:h-96 md:h-[450px] lg:h-[550px]"
-      />
-    </section>
-  );
-  //   }
-
-  //   if (!trailer.items) return null;
-
-  return (
-    <section className="pt-6">
-      <p className="head2 mb-2">예고편</p>
-
-      {/* <TrailerIframe trailer={trailer} movieTitle={movieDetail.title} /> */}
+      <TrailerIframe trailer={trailer} movieTitle={movieDetail.title} />
     </section>
   );
 };
