cd72811d8e4ee4e344c8facc6c31073ac01c2a87
H0onnn <dukei2017@naver.com>
2024-06-25 23:55:50 +0900

    feat : 모바일 기기용 헤더 메뉴 버튼 시트 구현

---
+++ app/shared/components/layout/header-menu-button.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { getServerSession } from "../../utils";
+
+import { Menu, Inbox } from "lucide-react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetFooter,
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+  Flex,
+  Button,
+} from "@/app/shared/components";
+import { MyActivities, SignoutButton } from "@/app/features/user/components";
+import { ThemeSelectButtons } from "../theme";
+
+export const HeaderMenuButton = async () => {
+  const session = await getServerSession();
+  const user = session?.user;
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild className="sm-max:flex md:hidden ml-4">
+        <Menu role="button" size={32} />
+      </SheetTrigger>
+      <SheetContent side="right" className="flex flex-col gap-8">
+        <SheetHeader>
+          <Flex align="center" className="space-x-3">
+            <Avatar className="border border-border border-solid w-12 h-12">
+              <AvatarImage src={user?.image as string} />
+              <AvatarFallback>{user?.name}</AvatarFallback>
+            </Avatar>
+
+            <SheetTitle>반가워요, {user?.name}님!</SheetTitle>
+          </Flex>
+        </SheetHeader>
+
+        <MyActivities className="p-0" />
+
+        <Button type="button" variant="outline" className="w-full" asChild>
+          <Link href={`/user/info/${user?.id}`}>
+            <Flex align="center" className="space-x-2">
+              <Inbox size={16} />
+              <span>내 정보</span>
+            </Flex>
+          </Link>
+        </Button>
+
+        <ThemeSelectButtons />
+
+        <SheetFooter className="absolute bottom-6 right-6">
+          <SignoutButton />
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
--- app/shared/components/layout/index.ts
+++ app/shared/components/layout/index.ts
@@ -1,2 +1,3 @@
 export * from "./header";
 export * from "./page-layout";
+export * from "./header-menu-button";
