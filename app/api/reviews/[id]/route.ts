3231c0e3428c78dd83a2a18c9233e0fb03281bbb
H0onnn <dukei2017@naver.com>
2024-06-28 14:47:26 +0900

    fix : req type을 NextApiRequest로 수정

--- app/api/reviews/[id]/route.ts
+++ app/api/reviews/[id]/route.ts
@@ -1,9 +1,10 @@
 import { getReviewsByMovie } from "@/app/features/review/apis";
+import { NextApiRequest } from "next";
 
 export async function GET(
-  req: Request,
+  req: NextApiRequest, // eslint-disable-line
   { params }: { params: { id: string } },
-) {
+): Promise<Response> {
   const movieId = params.id;
 
   const reviews = await getReviewsByMovie(movieId);
