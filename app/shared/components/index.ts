c18da9ceb148b165e2a73fd7f7af792fd9f900b0
H0onnn <dukei2017@naver.com>
2024-07-01 15:58:38 +0900

    rename : 폴더 구조 재구성

--- app/shared/components/index.ts
+++ app/shared/components/index.ts
@@ -1,5 +1,4 @@
 export * from "./ui";
 export * from "./layout";
-export * from "./blocks";
 export * from "./fallback";
 export * from "./async";
--- app/shared/components/layout/index.ts
+++ app/shared/components/layout/index.ts
@@ -1,2 +1,3 @@
 export { PageLayout } from "./page-layout";
 export { MainHeader } from "./main-header";
+export { Footer } from "./footer";
